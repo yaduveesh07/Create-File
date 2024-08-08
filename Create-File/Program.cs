b4f96f51f12a31b8bbf53138921bc4d2bfdb88a5
Yaduveesh Pratap Singh <yaduveeshp.singh@grovvey.com>
2024-08-08 09:24:39 +0530

    Add project files.

---
+++ Create-File/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class CreateFile
+{
+    public static void Main()
+    {
+        string fileName = @"testfile.txt";
+
+        try
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            Console.Write("\n\n Create a file in the disk if it exists:\n");
+            Console.Write("-----------------------------------------------\n");
+
+            using (FileStream fileStr = File.Create(fileName))
+            {
+                Console.WriteLine(" A file created with name testfile.txt\n\n");
+            }
+        }
+        catch (Exception MyExcep)
+        {
+            Console.WriteLine(MyExcep.ToString());
+        }
+    }
+}
